b0af613627e184e708eb6ffdad00a43977110daf
Cansu Akcora <cansu.akcora@vidizayn.com>
2022-10-15 13:05:40 +0300

    person-numbers-add

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 let saveEl = document.getElementById("save-el")
 let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+
 if( gecmisBilgi !== null) {
     saveEl.textContent = localStorage.getItem("gecmisBilgi")
 } else {
     saveEl.textContent = ""
 }
 
-
+let total = document.querySelector(".total-number")
 let countEl = document.getElementById("count-el")
 let count = 0
+let result = 0;
 
 function increment() {
     count += 1
     countEl.textContent = count
+
 }
 
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    result += count
+    total.textContent = result
+
     localStorage.setItem( "gecmisBilgi", saveEl.textContent )
-    countEl.textContent = 0
-    count = 0
+
+     countEl.textContent = 0
+     count = 0
+ 
 }
 
